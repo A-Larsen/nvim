de47c74bdedc9f1703e20fb5276474455d4006e9
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-16 23:29:38 -0400

    more progress

--- lua/nyquist/global.lua
+++ lua/nyquist/global.lua
@@ -27,10 +27,17 @@ function! AdjustFontSize(amount)
   :execute "GuiFont! Consolas:h" . s:fontsize
 endfunction
 
+function! SetFontSize(amount)
+  let s:fontsize = a:amount
+  :execute "GuiFont! Consolas:h" . s:fontsize
+endfunction
+
 nnoremap <c-z> <nop>
 
-noremap <silent> <C-ScrollWheelUp> :call AdjustFontSize(1)<CR><c-w>=
-noremap <silent> <C-ScrollWheelDown> :call AdjustFontSize(-1)<CR><c-w>=
-inoremap <silent> <C-ScrollWheelUp> <Esc>:call AdjustFontSize(1)<CR><c-w>=a
-inoremap <silent> <C-ScrollWheelDown> <Esc>:call AdjustFontSize(-1)<CR><c-w>=a
+noremap <silent> <C-=> :call AdjustFontSize(1)<CR><c-w>=
+noremap <silent> <C--> :call AdjustFontSize(-1)<CR><c-w>=
+inoremap <silent> <C-+> <Esc>:call AdjustFontSize(1)<CR><c-w>=a
+inoremap <silent> <C--> <Esc>:call AdjustFontSize(-1)<CR><c-w>=a
+inoremap <silent> <C-0> <Esc>:call SetFontSize(12)<cr><c-w>=a
+nnoremap <silent> <C-0> :call SetFontSize(12)<cr><c-w>=
 ]]
